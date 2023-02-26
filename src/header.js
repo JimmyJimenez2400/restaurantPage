03f978fa2e6b006150b3e6bc448999ad7eb2c702
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-02-25 20:45:11 -0500

    Add Functions To Create HTML Elements And Include Style Sheet

--- src/header.js
+++ src/header.js
@@ -2,5 +2,36 @@ export function header() {
   const headerContainer = document.createElement('div');
   headerContainer.setAttribute('id', 'header');
 
+  headerContainer.appendChild(navbar());
   return headerContainer;
+}
+
+function navbar() {
+  const navbar = document.createElement('nav');
+  navbar.setAttribute('class', 'navbar');
+
+  const unorderedList = document.createElement('ul');
+  unorderedList.setAttribute('class', 'unorderedList');
+  navbar.appendChild(unorderedList);
+
+  unorderedList.appendChild(createHTMLElement('li', 'class', 'list-item-1'));
+  unorderedList.appendChild(createHTMLElement('li', 'class', 'list-item-2'));
+  unorderedList.appendChild(createHTMLElement('li', 'class', 'list-item-3'));
+  unorderedList.appendChild(createHTMLElement('li', 'class', 'list-item-4'));
+
+  const listItems = unorderedList.querySelectorAll('li');
+  listItems[0].appendChild(
+    createHTMLElement('a', 'class', 'anchor-tag', 'Home')
+  );
+  // .appendChild(createHTMLElement('a', 'class', 'anchor-item', 'Home');
+
+  return navbar;
+}
+
+function createHTMLElement(element, type, className, content) {
+  const elementMade = document.createElement(element);
+  elementMade.setAttribute(type, className);
+  elementMade.textContent = content;
+
+  return elementMade;
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { header } from './header.js';
+import './style.css';
+
 
 function component() {
   //import functions or modules in here
